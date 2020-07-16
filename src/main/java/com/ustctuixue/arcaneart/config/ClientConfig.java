667e6a39daa9e0931a5437ffe8380c55f04f0ae0
TowardtheStars <hty19980402@qq.com>
2020-07-16 10:51:02 +0800

    注册配置文件

---
+++ src/main/java/com/ustctuixue/arcaneart/config/ClientConfig.java
@@ -0,0 +1,11 @@
+package com.ustctuixue.arcaneart.config;
+
+import net.minecraftforge.common.ForgeConfigSpec;
+
+public class ClientConfig
+{
+    public ClientConfig(ForgeConfigSpec.Builder builder)
+    {
+
+    }
+}
---
+++ src/main/java/com/ustctuixue/arcaneart/config/CommonConfig.java
@@ -0,0 +1,11 @@
+package com.ustctuixue.arcaneart.config;
+
+import net.minecraftforge.common.ForgeConfigSpec;
+
+public class CommonConfig
+{
+    public CommonConfig(ForgeConfigSpec.Builder builder)
+    {
+
+    }
+}
