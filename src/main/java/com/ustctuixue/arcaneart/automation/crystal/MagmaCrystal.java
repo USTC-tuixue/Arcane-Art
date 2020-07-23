d2a701a3883dac56f4f8b75faabccc05b5ce9d90
BuildTools <unconfigured@null.spigotmc.org>
2020-07-23 12:44:17 +0800

    Add Magma Crystal. Easy work.

---
+++ src/main/java/com/ustctuixue/arcaneart/automation/crystal/MagmaCrystal.java
@@ -0,0 +1,28 @@
+package com.ustctuixue.arcaneart.automation.crystal;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.material.Material;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.IBlockReader;
+
+import javax.annotation.Nullable;
+
+public class MagmaCrystal extends Block{
+    public MagmaCrystal(){
+        super(Properties.create(Material.ROCK).hardnessAndResistance(5));
+    }
+
+    @Override
+    public boolean hasTileEntity(BlockState state) {
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public TileEntity createTileEntity(BlockState state, IBlockReader world) {
+        return new MagmaCrystalTileEntity();
+    }
+
+}
+
---
+++ src/main/java/com/ustctuixue/arcaneart/automation/crystal/MagmaCrystalTileEntity.java
@@ -0,0 +1,34 @@
+package com.ustctuixue.arcaneart.automation.crystal;
+
+import com.udojava.evalex.Expression;
+import com.ustctuixue.arcaneart.automation.AutomationConfig;
+import com.ustctuixue.arcaneart.automation.AutomationRegistry;
+import com.ustctuixue.arcaneart.automation.EnvHelper;
+import net.minecraft.block.material.Material;
+import net.minecraft.util.math.BlockPos;
+
+import java.math.BigDecimal;
+
+public class MagmaCrystalTileEntity extends AbstractCollectiveCrystalTileEntity {
+    public MagmaCrystalTileEntity() {
+        super(AutomationRegistry.MAGMA_CRYSTAL_TILEENTITY.get());
+    }
+
+    @Override
+    public double crystalRegenRatio(){
+        //下方5*5正方形去掉四个角的范围内均是岩浆，才会工作（药物配置范围）
+        BlockPos pos = this.getPos().down(1);
+        for(int i = -1; i <= 2; i++){
+            //这里为了加速免去(-2, -2)的获取
+            for(int j = -2; j <= 2; j++){
+                BlockPos newpos = new BlockPos(pos.getX() + i, pos.getY(), pos.getZ() + j);
+                assert world != null;
+                if (((i==-2)||(i==2))&&((j==-2)||(j==2)))
+                    continue;
+                if (world.getBlockState(newpos).getMaterial() != Material.LAVA)
+                    return 0.0D;
+            }
+        }
+        return (AutomationConfig.Crystal.MAGMA_CRYSTAL_REGEN_RATIO.get())/20;//从每秒恢复速率转换为每tick
+    }
+}
