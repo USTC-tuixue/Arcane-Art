5a6a58b8aefe950ca396394d3ce6c0756a5beebf
Pscgylotti <57549504+Pscgylotti@users.noreply.github.com>
2020-07-28 00:03:58 +0800

    Update ItemRegistry.java

--- src/main/java/com/ustctuixue/arcaneart/misc/item/ItemRegistry.java
+++ src/main/java/com/ustctuixue/arcaneart/misc/item/ItemRegistry.java
@@ -11,7 +11,22 @@ import net.minecraftforge.registries.ForgeRegistries;
 
 public class ItemRegistry {
 	  public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, "arcaneart");
-	  public static RegistryObject<Item> bookShelf = ITEMS.register("book_shelf", () -> {
-		  return new BlockItem(BlockRegistry.bookShelf.get(), new Item.Properties().group(ArcaneArt.ARCANE_ART_ITEM_GROUP));
+	  public static RegistryObject<Item> bookShelfAcacia = ITEMS.register("acacia_book_shelf", () -> {
+		  return new BlockItem(BlockRegistry.bookShelfAcacia.get(), new Item.Properties().group(ArcaneArt.ARCANE_ART_ITEM_GROUP));
+		});
+	  public static RegistryObject<Item> bookShelfBirch = ITEMS.register("birch_book_shelf", () -> {
+		  return new BlockItem(BlockRegistry.bookShelfBirch.get(), new Item.Properties().group(ArcaneArt.ARCANE_ART_ITEM_GROUP));
+		});
+	  public static RegistryObject<Item> bookShelfDarkOak = ITEMS.register("dark_oak_book_shelf", () -> {
+		  return new BlockItem(BlockRegistry.bookShelfDarkOak.get(), new Item.Properties().group(ArcaneArt.ARCANE_ART_ITEM_GROUP));
+		});
+	  public static RegistryObject<Item> bookShelfJungle = ITEMS.register("jungle_book_shelf", () -> {
+		  return new BlockItem(BlockRegistry.bookShelfJungle.get(), new Item.Properties().group(ArcaneArt.ARCANE_ART_ITEM_GROUP));
+		});
+	  public static RegistryObject<Item> bookShelfOak = ITEMS.register("oak_book_shelf", () -> {
+		  return new BlockItem(BlockRegistry.bookShelfOak.get(), new Item.Properties().group(ArcaneArt.ARCANE_ART_ITEM_GROUP));
+		});
+	  public static RegistryObject<Item> bookShelfSpruce = ITEMS.register("spruce_book_shelf", () -> {
+		  return new BlockItem(BlockRegistry.bookShelfSpruce.get(), new Item.Properties().group(ArcaneArt.ARCANE_ART_ITEM_GROUP));
 		});
 }
