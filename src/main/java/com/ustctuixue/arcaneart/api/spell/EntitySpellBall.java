8fc7b4dbbb5f98f61c512ec082dcb0738b98bdd9
BuildTools <unconfigured@null.spigotmc.org>
2020-08-08 17:50:47 +0800

    每天摸一点……

--- src/main/java/com/ustctuixue/arcaneart/api/spell/EntitySpellBall.java
+++ src/main/java/com/ustctuixue/arcaneart/api/spell/EntitySpellBall.java
@@ -9,13 +9,12 @@ import lombok.Setter;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.projectile.DamagingProjectileEntity;
-import net.minecraft.entity.projectile.ThrowableEntity;
+import net.minecraft.entity.projectile.ProjectileHelper;
 import net.minecraft.nbt.CompoundNBT;
-import net.minecraft.nbt.ListNBT;
 import net.minecraft.network.IPacket;
 import net.minecraft.network.play.server.SSpawnObjectPacket;
 import net.minecraft.util.Direction;
+import net.minecraft.util.math.RayTraceContext;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
@@ -24,18 +23,20 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.util.LazyOptional;
-import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 
 import javax.annotation.Nonnull;
-import java.lang.reflect.Field;
 
 public class EntitySpellBall extends Entity{
     //建议直接转extends Entity，告辞
 
+    @Getter @Setter
     public LivingEntity shootingEntity;
 
     @Getter @Setter
     protected double gravityFactor;//0<=gravityFactor<=1.0. preserved. 为2.0的受重力影响法球预留
+
+    public int ticksAlive = 0;//生存时间的计时器。
+    public int maxTimer = 1000;//最大不衰减飞行时间，后面要换成从cfg读
     
     protected void registerData() {
     }
@@ -46,7 +47,7 @@ public class EntitySpellBall extends Entity{
         Vec3d vec3d = this.getMotion();
         compound.put("direction", this.newDoubleNBTList(new double[]{vec3d.x, vec3d.y, vec3d.z}));
         //compound.put("power", this.newDoubleNBTList(new double[]{this.accelerationX, this.accelerationY, this.accelerationZ}));
-        //compound.putInt("life", this.ticksAlive);
+        compound.putInt("life", this.ticksAlive);
     }
 
     public void readAdditional(CompoundNBT compound) {
@@ -115,12 +116,15 @@ public class EntitySpellBall extends Entity{
     private EntitySpellBall(Builder builder){
         super(APIRegistries.Entities.SPELL_BALL_TYPE, builder.world);
         this.setLocationAndAngles(builder.x, builder.y, builder.z, builder.yaw, builder.pitch);
-
+        this.setGravityFactor(builder.gravityFactor);
+        this.setShootingEntity(builder.shooter);
+        this.setMotion(new Vec3d(builder.vx,builder.vy,builder.vz));
     }
 
     public static class Builder{
         private World world;
         private double x, y, z;
+        private double vx, vy, vz;
         private double gravityFactor;
         private LivingEntity shooter;
         private float yaw, pitch;
@@ -132,6 +136,12 @@ public class EntitySpellBall extends Entity{
              this.x = x;
              this.y = y;
              this.z = z;
+             return this;
+         }
+         public Builder motion(double vx, double vy, double vz){
+             this.vx = vx;
+             this.vy = vy;
+             this.vz = vz;
              return this;
          }
          public Builder shooter(LivingEntity shooter){
@@ -173,12 +183,20 @@ public class EntitySpellBall extends Entity{
     protected void onImpact(RayTraceResult result)
     {
         //super.onImpact(result);
+
         if (!this.world.isRemote){
             if(!this.translatedSpellProvider.hasSpell()){
                 //插入作为能量传输手段的逻辑
-                //和棱镜碰撞时转向（专门处理和xyz轴对齐的情况以提高效率，是否要考虑非全反射？）
+                //和棱镜碰撞时转向（专门处理和xyz轴对齐的情况以提高效率）
+                //棱镜分两种：直角棱镜和分光棱镜
+                //不考虑任何折射，摸了
                 //和MPStorage的te碰撞时赋予其能量
-
+                if(result.getType() == RayTraceResult.Type.BLOCK){
+                    //碰上方块了
+                }
+                else if(result.getType() == RayTraceResult.Type.ENTITY) {
+                    //给实体补充能量
+                }
                 return;
             };
             TranslatedSpell spell = this.translatedSpellProvider.getSpell();
@@ -186,19 +204,45 @@ public class EntitySpellBall extends Entity{
         }
     }
 
-    //static Field TICKS_ALIVE_FIELD;
-    public int aliveTimer = 0;//生存时间的计时器。因为原生的是private所以还是自己实现一套比较好，或许还能算算距离
-    public int maxTimer = 1000;//最大不衰减飞行时间，后面换成从cfg读
-
     @Override
     public void tick()
     {
         super.tick();
-        if (!this.world.isRemote){
+
+        if (!this.world.isRemote) {
+
+            ++this.ticksAlive;
+            RayTraceResult raytraceresult = ProjectileHelper.rayTrace(this, true, this.ticksAlive >= 25, this.shootingEntity, RayTraceContext.BlockMode.COLLIDER);
+            //这个includeShooter大概率是指在什么情况下允许射出投掷物的实体被投掷物击中，但是逻辑比较复杂，我读不太懂。此处照抄了DamagingProjectileEntity的逻辑
+
+            if (raytraceresult.getType() != RayTraceResult.Type.MISS && !net.minecraftforge.event.ForgeEventFactory.onProjectileImpact(this, raytraceresult)) {
+                this.onImpact(raytraceresult);
+            }
+
+            Vec3d vec3d = this.getMotion();
+            double d0 = this.getPosX() + vec3d.x;
+            double d1 = this.getPosY() + vec3d.y;
+            double d2 = this.getPosZ() + vec3d.z;
+            ProjectileHelper.rotateTowardsMovement(this, 0.2F);
+            //float f = this.getMotionFactor();
+            /*
+            if (this.isInWater()) {
+                for(int i = 0; i < 4; ++i) {
+                    float f1 = 0.25F;
+                    this.world.addParticle(ParticleTypes.BUBBLE, d0 - vec3d.x * 0.25D, d1 - vec3d.y * 0.25D, d2 - vec3d.z * 0.25D, vec3d.x, vec3d.y, vec3d.z);
+                }
+                f = 0.8F;
+            }
+            */
+
+            //this.setMotion(vec3d.add(this.accelerationX, this.accelerationY, this.accelerationZ).scale((double)f));
+            //this.world.addParticle(this.getParticle(), d0, d1 + 0.5D, d2, 0.0D, 0.0D, 0.0D);
+            this.setPosition(d0, d1, d2);
+
             //TODO
             //随时间而增加的能量消耗写在这
-            aliveTimer ++;
-            if(aliveTimer > maxTimer){
+            ticksAlive++;
+            if(ticksAlive > maxTimer){
                 //指数降低mp存量
             }
             this.translatedSpellProvider.getCompiled(source).executeOnHold(source);
