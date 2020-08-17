70e771808d6e9af4477b9a664aa7eaac548b1699
Pscgylotti <57549504+Pscgylotti@users.noreply.github.com>
2020-08-17 10:08:40 +0800

    Create ItemStaff.java

---
+++ src/main/java/com/ustctuixue/arcaneart/misc/item/ItemStaff.java
@@ -0,0 +1,15 @@
+package com.ustctuixue.arcaneart.misc.item;
+
+import com.ustctuixue.arcaneart.ArcaneArt;
+import com.ustctuixue.arcaneart.api.spell.ItemSpellCaster;
+
+import net.minecraft.item.Item.Properties;
+
+public class ItemStaff extends ItemSpellCaster{
+
+	public ItemStaff() {
+		super(new Properties().group(ArcaneArt.ARCANE_ART_ITEM_GROUP),2);
+			// TODO Auto-generated constructor stub
+	}
+
+}
