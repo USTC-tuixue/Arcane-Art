afc994038f24ec488147a9178a82ba35dbed18d0
TowardtheStars <hty19980402@qq.com>
2020-08-20 00:56:42 +0800

    修复了法术不能生成法球的bug

--- src/main/java/com/ustctuixue/arcaneart/spell/spell/SummonSpellBallSpell.java
+++ src/main/java/com/ustctuixue/arcaneart/spell/spell/SummonSpellBallSpell.java
@@ -1,11 +1,30 @@
 package com.ustctuixue.arcaneart.spell.spell;
 
 import com.mojang.brigadier.StringReader;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.ustctuixue.arcaneart.api.spell.ITranslatedSpellProvider;
+import com.ustctuixue.arcaneart.api.spell.TranslatedSpell;
+import com.ustctuixue.arcaneart.api.spell.entityspellball.EntitySpellBall;
 import com.ustctuixue.arcaneart.api.spell.interpreter.ISpell;
+import com.ustctuixue.arcaneart.api.spell.interpreter.Interpreter;
 import com.ustctuixue.arcaneart.api.spell.interpreter.SpellCasterSource;
+import com.ustctuixue.arcaneart.api.spell.interpreter.SpellContainer;
+import com.ustctuixue.arcaneart.api.spell.interpreter.argument.Variable;
+import com.ustctuixue.arcaneart.api.spell.interpreter.argument.clause.FromClause;
+import com.ustctuixue.arcaneart.api.spell.interpreter.argument.clause.TowardsClause;
+import com.ustctuixue.arcaneart.api.spell.interpreter.argument.raytrace.DirectionBuilder;
+import com.ustctuixue.arcaneart.api.spell.interpreter.argument.raytrace.DirectionVariableArgument;
+import com.ustctuixue.arcaneart.api.spell.translator.RawSpell;
+import com.ustctuixue.arcaneart.spell.SpellModuleRegistries;
 
 public class SummonSpellBallSpell implements ISpell
 {
+    TranslatedSpell spell;
+    Variable<DirectionBuilder> vec;
+    double speed;
+    final ITranslatedSpellProvider provider = new ITranslatedSpellProvider.Impl();
+    int maxLoop;
+
     /**
      * Get spell complexity according to spell source
      *
@@ -15,7 +34,11 @@ public class SummonSpellBallSpell implements ISpell
     @Override
     public double getComplexityBase(SpellCasterSource source)
     {
-        return 0;
+        SpellContainer compiled = this.provider.getCompiled(source);
+        double cost = compiled.preProcess.stream().mapToDouble(iSpell -> iSpell.getComplexityBase(source)).sum();
+        cost += compiled.onHold.stream().mapToDouble(iSpell -> iSpell.getComplexityBase(source)).sum() * this.maxLoop;
+        cost += compiled.onRelease.stream().mapToDouble(iSpell -> iSpell.getComplexityBase(source)).sum();
+        return cost;
     }
 
     /**
@@ -25,7 +48,12 @@ public class SummonSpellBallSpell implements ISpell
     @Override
     public double getManaCostBase(SpellCasterSource source)
     {
-        return 0;
+        SpellContainer compiled = this.provider.getCompiled(source);
+        double cost = compiled.preProcess.stream().mapToDouble(iSpell -> iSpell.getManaCostBase(source)).sum();
+        cost += compiled.onHold.stream().mapToDouble(iSpell -> iSpell.getManaCostBase(source)).sum() * this.maxLoop;
+        cost += compiled.onRelease.stream().mapToDouble(iSpell -> iSpell.getManaCostBase(source)).sum();
+        cost += 10;
+        return cost;
     }
 
     /**
@@ -36,7 +64,24 @@ public class SummonSpellBallSpell implements ISpell
     @Override
     public void execute(SpellCasterSource source)
     {
+        EntitySpellBall spellBall;
 
+        if (this.vec == null)
+        {
+            spellBall = new EntitySpellBall.Builder(source.getWorld())
+                    .setFullMP(this.getManaCostBase(source))
+                    .build();
+        }
+        else
+        {
+            spellBall = new EntitySpellBall.Builder(source.getWorld())
+                    .motion(source.getVariable(this.vec.getName(), this.vec.getType()).build(source).next().scale(this.speed))
+                    .setFullMP(this.getManaCostBase(source))
+                    .build();
+        }
+        spellBall.translatedSpellProvider.setSpell(this.spell);
+
+        source.getWorld().addEntity(spellBall);
     }
 
     /**
@@ -48,12 +93,53 @@ public class SummonSpellBallSpell implements ISpell
     @Override
     public boolean parse(StringReader reader)
     {
-        return false;
+        reader.skipWhitespace();
+        try
+        {
+            this.vec = new TowardsClause().parse(reader);
+        }catch (CommandSyntaxException ignored)
+        {
+            this.vec = null;
+        }
+
+        reader.skipWhitespace();
+        try
+        {
+            if (reader.readStringUntil(' ').equals(SpellModuleRegistries.SpellKeyWords.WITH_SPEED.get().toString()))
+            {
+                this.speed = reader.readDouble();
+            }
+        }
+        catch (CommandSyntaxException e)
+        {
+            this.speed = 0;
+        }
+        reader.skipWhitespace();
+        try
+        {
+            this.spell = TranslatedSpell.fromRawSpell(new RawSpell("", reader.readString(), null));
+            this.provider.setSpell(this.spell);
+        }
+        catch (CommandSyntaxException e)
+        {
+            e.printStackTrace();
+            return false;
+        }
+        reader.skipWhitespace();
+        try
+        {
+            this.maxLoop = reader.readInt();
+        }catch (CommandSyntaxException e)
+        {
+            this.maxLoop = 20;
+        }
+
+        return true;
     }
 
     @Override
     public double guessManaCost(SpellCasterSource source)
     {
-        return 0;
+        return this.getManaCost(source);
     }
 }
